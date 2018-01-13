e622587c1d786d3f0f1cce20f8eeb9ff9035c419
somkiat <somkiat.p@gmail.com>
2018-01-13 22:26:22 +0700

    Demo hello service with spring boot

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting hello-service "
+echo "********************************************************"
+java -jar /usr/local/hello-service/@project.build.finalName@.jar
---
+++ src/main/java/demo/Application.java
@@ -0,0 +1,26 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(value="hello")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping(value="/{firstName}/{lastName}")
+    public String hello( @PathVariable("firstName") String firstName,
+                         @PathVariable("lastName") String lastName) {
+
+        return String.format("{\"message\":\"Hello %s %s\"}", firstName, lastName);
+    }
+
+}
